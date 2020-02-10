eca05552cf01052b4315ad2542b35020a77423ed
reza jm <shadow.rexa@gmail.com>
2020-02-10 17:30:15 +0330

    set config for React router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {Route , Switch} from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage.component";
 import './App.css';
 
+const hatpage = () =>(
+  <div>
+    <h1>hats</h1>
+  </div>
+)
+
+
+
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch> 
+       <Route exact path='/' component ={HomePage} />
+       <Route exact path='/shop/hats' component ={hatpage} />
+      </Switch>
+    </div>
   );
 }
 
--- src/Pages/HomePage/HomePage.component.jsx
+++ src/Pages/HomePage/HomePage.component.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 import './HomePage.styles.scss';
 import Directory from '../../Components/directory-menu/directory-menu.component';
 
-const HomePage = () => (
-
-    
+const HomePage = (props) =>{
+    console.log(props);
+ return(   
+     
             <div className="homepage">
                 <Directory />
                 </div>  
+ )        
                 
-                
-)
+}
                 
                 
                 export default HomePage;
